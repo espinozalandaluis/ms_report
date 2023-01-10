a21783a08ea82ad2900bbd584086620f4b0fb3f7
aespinozar19 <aespinozar19@gmail.com>
2023-01-10 18:31:00 -0500

    a

--- src/main/java/com/bootcamp/java/report/service/productClient/ProductClientServiceImpl.java
+++ src/main/java/com/bootcamp/java/report/service/productClient/ProductClientServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bootcamp.java.report.service.productClient;
 
+import com.bootcamp.java.report.common.exceptionHandler.FunctionalException;
 import com.bootcamp.java.report.converter.ProductClientConvert;
 import com.bootcamp.java.report.dto.ProductClientDTO;
 import com.bootcamp.java.report.repository.ProductClientRepository;
@@ -26,6 +27,16 @@ public class ProductClientServiceImpl implements ProductClientService{
     @Override
     public Mono<ProductClientDTO> create(ProductClientDTO productClientDTO) {
         log.info("Registrar ProductClientDTO en BD");
-        return Mono.just(productClientDTO);
+
+        return productClientRepository.findById(productClientDTO.getId())
+                .switchIfEmpty(productClientRepository.save(ProductClientConvert.DTOToEntity(productClientDTO)))
+                .flatMap(productClient -> {
+                    //Actualizar Balance
+                    productClient.setBalance(productClientDTO.getBalance());
+                    return productClientRepository.save(productClient)
+                            .map(prdcli -> ProductClientConvert.EntityToDTO(prdcli));
+                });
+
+        //return Mono.just(productClientDTO);
     }
 }
--- src/main/java/com/bootcamp/java/report/service/transaction/TransactionServiceImpl.java
+++ src/main/java/com/bootcamp/java/report/service/transaction/TransactionServiceImpl.java
@@ -3,6 +3,7 @@ package com.bootcamp.java.report.service.transaction;
 import com.bootcamp.java.report.converter.ProductClientConvert;
 import com.bootcamp.java.report.converter.TransactionConvert;
 import com.bootcamp.java.report.dto.TransactionDTO;
+import com.bootcamp.java.report.entity.Transaction;
 import com.bootcamp.java.report.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,11 +31,15 @@ public class TransactionServiceImpl implements TransactionService{
     @Autowired
     ProductClientConvert productClientConvert;
 
-
     @Override
     public Mono<TransactionDTO> register(TransactionDTO transactionDTO) {
 
         log.info("Registrar transaction en BD");
-        return Mono.just(transactionDTO);
+
+        Transaction transaction = TransactionConvert.DTOtoEntity(transactionDTO);
+        return transactionRepository.save(TransactionConvert.DTOtoEntity(transactionDTO))
+                .map(trx -> TransactionConvert.EntityToDTO(trx));
+
+        //return Mono.just(transactionDTO);
     }
 }
