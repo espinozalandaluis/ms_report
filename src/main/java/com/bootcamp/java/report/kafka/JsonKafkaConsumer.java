c2fa37d36780de7771e6994c0125d51f8b434412
aespinozar19 <aespinozar19@gmail.com>
2023-01-10 15:33:54 -0500

    actualizacion

--- src/main/java/com/bootcamp/java/report/kafka/JsonKafkaConsumer.java
+++ src/main/java/com/bootcamp/java/report/kafka/JsonKafkaConsumer.java
@@ -1,6 +1,7 @@
 package com.bootcamp.java.report.kafka;
 
 import com.bootcamp.java.kafka.ProductClientDTO;
+import com.bootcamp.java.kafka.TransactionDTO;
 import com.bootcamp.java.kafka.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,18 +13,28 @@ public class JsonKafkaConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
 
+    /*
+
+
     @KafkaListener(topics = "${spring.kafka.topic-transaction.name}",
             groupId = "${spring.kafka.consumer.group-id}"
             )
     public void consume(User user){
         LOGGER.info(String.format("Json message recieved -> %s", user.toString()));
-    }
+    }*/
 
     @KafkaListener(topics = "${spring.kafka.topic.productclient.name}",
             groupId = "${spring.kafka.consumer.group-id}"
     )
     public void consumeProductClient(ProductClientDTO productClientDTO){
-        LOGGER.info(String.format("consumeProductClient Json message recieved -> %s", productClientDTO.toString()));
+        LOGGER.info(String.format("consumeProductClientDTO message recieved -> %s", productClientDTO.toString()));
+    }
+
+    @KafkaListener(topics = "${spring.kafka.topic-transaction.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consumeTransaction(TransactionDTO transactionDTO){
+        LOGGER.info(String.format("consumeTransactionDTO message recieved -> %s", transactionDTO.toString()));
     }
 
 }
